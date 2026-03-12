4c6660bc83c7b7d731724d8cec913dd47ca21eee
Fredrik Boglind <fredrik.boglind@gmail.com>
2026-03-12 15:40:45 +0100

    feat: add preliminary results notebook for BNT

--- src/thesis_project/embeddings/__init__.py
+++ src/thesis_project/embeddings/__init__.py
@@ -1,1 +1,5 @@
-"""init.py"""
+"""Embeddings module for text encoding."""
+
+from .encoder import Embedder, KBBertEmbedder, MockEmbedder
+
+__all__ = ["Embedder", "KBBertEmbedder", "MockEmbedder"]
--- src/thesis_project/scoring/__init__.py
+++ src/thesis_project/scoring/__init__.py
@@ -1,1 +1,13 @@
-"""init.py"""
+"""Scoring module for computing similarity scores."""
+
+from .graded_scorer import (
+    GradedScorer,
+    compute_cosine_similarity,
+    compute_similarity_scores,
+)
+
+__all__ = [
+    "GradedScorer",
+    "compute_cosine_similarity",
+    "compute_similarity_scores",
+]
